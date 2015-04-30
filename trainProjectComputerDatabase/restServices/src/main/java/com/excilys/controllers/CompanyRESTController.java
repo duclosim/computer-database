61ad130adff726a29137c65521d4e0908fe3c911
duclosim <sduclos@excilys.com>
2015-04-30 15:29:00 +0200

    doc(restServices) : Edition of the javadoc.

--- trainProjectComputerDatabase/restServices/src/main/java/com/excilys/controllers/CompanyRESTController.java
+++ trainProjectComputerDatabase/restServices/src/main/java/com/excilys/controllers/CompanyRESTController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.excilys.model.beans.Company;
 import com.excilys.services.CompanyService;
 
+/**
+ * This class exposes listing functions for Company objects from the database.
+ * @author excilys
+ *
+ */
 @RestController
 @RequestMapping("/companies")
 public class CompanyRESTController {
@@ -21,12 +26,23 @@ public class CompanyRESTController {
 	@Autowired
 	private CompanyService service;
 	
+	/**
+	 * This method return the company matching given id, 
+	 *   null if no objects are found.
+	 * @param id The id owned by the searched Company.
+	 * @return A Company with matching id, 
+	 *   null if no matching Company are found.
+	 */
 	@RequestMapping(value = "/getById", method = RequestMethod.GET)
 	public Company getById(@RequestParam(value = "id") Long id) {
 		LOG.trace("getById(" + id + ")");
 		return service.getById(id);
 	}
 	
+	/**
+	 * This method returns all of the Company objects from the database.
+	 * @return A list of all the Company objects from the database..
+	 */
     @RequestMapping(value = "/list",  method = RequestMethod.GET)
     public List<Company> getList() {
 		LOG.trace("list()");
--- trainProjectComputerDatabase/restServices/src/main/java/com/excilys/controllers/ComputerRESTController.java
+++ trainProjectComputerDatabase/restServices/src/main/java/com/excilys/controllers/ComputerRESTController.java
@@ -11,39 +11,68 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.excilys.binding.dtos.ComputerDTO;
-import com.excilys.binding.dtos.ComputerDTOMapper;
-import com.excilys.binding.dtos.PageDTO;
+import com.excilys.model.beans.Computer;
+import com.excilys.page.Page;
 import com.excilys.persistence.daos.ComputerColumn;
 import com.excilys.persistence.daos.OrderingWay;
 import com.excilys.services.ComputerService;
 
+/**
+ * This class exposes listing functions for Computer objects from the database.
+ * @author excilys
+ *
+ */
 @RestController
 @RequestMapping("/computers")
 public class ComputerRESTController {
 	private static final Logger LOG = LoggerFactory.getLogger(ComputerRESTController.class);
 	
 	@Autowired
-	private ComputerDTOMapper mapper;
-	@Autowired
 	private ComputerService service;
 	@Autowired
-	private PageDTO page;
+	private Page page;
     
+	/**
+	 * Add a new Computer to the database.
+	 * @param computer The computer to add in the database.
+	 */
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public void create(@RequestBody ComputerDTO computer) {
+    public void create(@RequestBody Computer computer) {
     	LOG.trace("create(" + computer + ")");
-    	service.create(mapper.DTOToBean(computer));
+    	service.create(computer);
     }
-	
+
+	/**
+	 * This method return the Computer matching given id, 
+	 *   null if no objects are found.
+	 * @param id The id owned by the searched Company.
+	 * @return A Company with matching id, 
+	 *   null if no matching Company are found.
+	 */
 	@RequestMapping(value = "/getById", method = RequestMethod.GET)
-	public ComputerDTO getById(@RequestParam(value = "id") Long id) {
+	public Computer getById(@RequestParam(value = "id") Long id) {
 		LOG.trace("getById(" + id + ")");
-		return mapper.BeanToDTO(service.getById(id));
+		return service.getById(id);
 	}
 	
+	/**
+	 * This method get a list formed by a part of the Computer table.
+	 *   It can take several arguments, namely for ordering 
+	 *   and name-based filtering.
+	 * @param nbItem The maximum number of objects to be retrieved, 
+	 *   10 by default.
+	 * @param name The String for name-based filtering of Computer objects, 
+	 *   empty by default.
+	 * @param col The column to perform ordering, empty by default, 
+	 *   null by default.
+	 * @param way The way to order Computer objects, null by default.
+	 * @param pageNum The number of the page to get the objects from, 
+	 *   1 by default.
+	 * @return A List of the Computer objects contained by the Page object
+	 *   which parameters have been set.
+	 */
     @RequestMapping(value = "/list",  method = RequestMethod.GET)
-    public List<ComputerDTO> getList(@RequestParam(value = "nbItem", defaultValue = "10") int nbItem,
+    public List<Computer> getList(@RequestParam(value = "nbItem", defaultValue = "10") int nbItem,
     		@RequestParam(value = "name", defaultValue = "") String name,
     		@RequestParam(value = "col", defaultValue = "computer.id") String col,
     		@RequestParam(value = "way", defaultValue = "ASC") String way,
@@ -69,12 +98,16 @@ public class ComputerRESTController {
     		}
     	}
     	page.setPageNum(pageNum);
-    	return page.getDtos();
+    	return page.getEntities();
     }
 
+    /**
+     * This method updates a Computer in the database.
+     * @param computer The Computer to be updated in the database.
+     */
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public void update(@RequestBody ComputerDTO computer) {
+    public void update(@RequestBody Computer computer) {
 		LOG.trace("update(" + computer + ")");
-		service.update(mapper.DTOToBean(computer));
+		service.update(computer);
     }
 }
